d6a33955830a61c81a71858e921c6adcbdc2180b
kimyoongwon <yg.kim2@okestro.com>
2022-06-24 16:16:41 +0900

    기타 : swagger 2.0 버전 적용시 에러문제로 인해 바코드 발급 프로젝트 재생성

---
+++ IssueBarcodeAPI/src/main/java/com/kakaopay/issueapi/IssueBarcodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.issueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueBarcodeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueBarcodeApiApplication.class, args);
+    }
+
+}
