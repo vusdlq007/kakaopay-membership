29f6550972db78974c530913ef72364cc21748ff
kimyoongwon <yg.kim2@okestro.com>
2022-06-24 21:09:37 +0900

    기능 : 포인트 적립 API 구현
    - 포인트 적립 기능 구현 및 테스트 완료
    *JPA를 사용한 더티체크로 업데이트 기능 구현.

--- EarnPointAPI/src/main/java/com/kakaopay/earnpointapi/api/vo/PointVo.java
+++ EarnPointAPI/src/main/java/com/kakaopay/earnpointapi/api/vo/PointVo.java
@@ -17,14 +17,10 @@ public class PointVo {
     @Column(name = "id")
     private Integer pointId;
 
-//    @JsonBackReference
-//    @OneToOne(mappedBy = "point")                              // 하나의 Member는 한개의 Point를 가질수 있기때문에 @OneToOne이 된다. 해당 키의 주인되는 엔티티는 외래키를 가지고 있기때문에 현재 테이블인 T_POINT가 된다.
     @JoinColumn
     private String barcode;
 
-//    @JsonBackReference
-//    @ManyToOne                                  // 하나의 Category는 여러개의 store을 가질수 있기때문에 T_STORE 관점에서는 @ManyToOne이 된다. 해당 키의 주인되는 엔티티는 외래키를 가지고 있기때문에 현재 테이블인 T_STORE가 된다.
-    @JoinColumn                                // 실제 외래키의 명을 제대로 넣어줘야한다.
+    @JoinColumn
     private Integer category;
 
     @Column(name = "point")
--- EarnPointAPI/src/main/java/com/kakaopay/earnpointapi/api/vo/StoreVo.java
+++ EarnPointAPI/src/main/java/com/kakaopay/earnpointapi/api/vo/StoreVo.java
@@ -27,5 +27,4 @@ public class StoreVo {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-
 }
